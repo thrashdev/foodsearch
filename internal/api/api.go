70544b77d6779e473667c7568190fa30bb9ca068
thrashdev <thrashdev@protonmail.com>
2024-12-14 17:14:14 +0500

    added restaurants endpoint

--- internal/api/api.go
+++ internal/api/api.go
@@ -18,44 +18,90 @@ type ApiConfig struct {
 }
 
 type Restaurant struct {
-	ID         uuid.UUID
-	GlovoRest  *models.GlovoRestaurant
-	YandexRest *models.YandexRestaurant
+	ID         uuid.UUID                `json:"id"`
+	GlovoRest  *models.GlovoRestaurant  `json:"glovo_restaurant"`
+	YandexRest *models.YandexRestaurant `json:"yandex_restaurant"`
 }
 
-func (cfg *ApiConfig) handlerGetRestaurants(w http.ResponseWriter, r *http.Request) {
+func (cfg *ApiConfig) HandlerGetRestaurants(w http.ResponseWriter, r *http.Request) {
+	cfg.Logger.Info("Serving all restaurants on /v1/restaurants")
 	ctx := context.Background()
 	restaurantRows, err := cfg.DB.GetAllRestaurants(ctx)
 	if err != nil {
 		cfg.Logger.Panicw("Error fetching restaurants from DB", "error", err)
 	}
+	restaurants := []Restaurant{}
+	for _, row := range restaurantRows {
+		restaurants = append(restaurants, serializeRestaurant(row))
+	}
+
+	utils.RespondWithJSON(w, 200, restaurants)
 }
 
-func serializeRestaurants(rows []database.GetAllRestaurantsRow) []Restaurant {
-	result := []Restaurant{}
-	for _, row := range rows {
-		var glovoRest *models.GlovoRestaurant
-		glovoRest = nil
-		glovoDeliveryFee, err := row.DeliveryFee.Float64Value()
-		if err != nil {
-			log.Fatalf("Error converting pgtype.Numeric to float64, err:%v", err)
-		}
-		if row.ID_2.Valid {
-			glovoRest = &models.GlovoRestaurant{
-				Restaurant: models.Restaurant{
-					ID:          row.ID_2.Bytes,
-					Name:        row.Name.String,
-					Address:     &row.Address.String,
-					DeliveryFee: &glovoDeliveryFee.Float64,
-					PhoneNumber: &row.PhoneNumber.String,
-				},
-			}
-		}
-		rest := Restaurant{
-			ID:        row.ID.Bytes,
-			GlovoRest: glovoRest,
-		}
-		result = append(result)
+func serializeRestaurant(row database.GetAllRestaurantsRow) Restaurant {
+	var glovoRest *models.GlovoRestaurant
+	glovoRest = nil
+
+	glovoDeliveryFee, err := row.DeliveryFee.Float64Value()
+	if err != nil {
+		log.Fatalf("Error converting pgtype.Numeric to float64, err:%v", err)
 	}
+
+	if row.ID_2.Valid {
+		glovoRest = &models.GlovoRestaurant{
+			Restaurant: models.Restaurant{
+				ID:          row.ID_2.Bytes,
+				Name:        row.Name.String,
+				Address:     &row.Address.String,
+				DeliveryFee: &glovoDeliveryFee.Float64,
+				PhoneNumber: &row.PhoneNumber.String,
+			},
+			GlovoApiStoreID:   int(row.GlovoApiStoreID.Int32),
+			GlovoApiAddressID: int(row.GlovoApiAddressID.Int32),
+			GlovoApiSlug:      row.GlovoApiSlug.String,
+		}
+	}
+
+	yandexAddress := ""
+	if row.Address_2.Valid {
+		yandexAddress = row.Address_2.String
+	}
+
+	yandexDeliveryFee := 0.0
+	floatValue, err := row.DeliveryFee_2.Float64Value()
+	if err != nil {
+		if floatValue.Valid {
+			yandexDeliveryFee = floatValue.Float64
+		}
+	}
+
+	yandexPhoneNumber := ""
+	if row.PhoneNumber_2.Valid {
+		yandexPhoneNumber = row.PhoneNumber.String
+	}
+
+	var yandexRest *models.YandexRestaurant
+	yandexRest = nil
+
+	if row.ID_3.Valid {
+		yandexRest = &models.YandexRestaurant{
+			Restaurant: models.Restaurant{
+				ID:          row.ID_3.Bytes,
+				Name:        row.Name_2.String,
+				Address:     &yandexAddress,
+				DeliveryFee: &yandexDeliveryFee,
+				PhoneNumber: &yandexPhoneNumber,
+			},
+			YandexApiSlug: row.YandexApiSlug.String,
+		}
+	}
+
+	rest := Restaurant{
+		ID:         row.ID.Bytes,
+		GlovoRest:  glovoRest,
+		YandexRest: yandexRest,
+	}
+
+	return rest
 
 }
